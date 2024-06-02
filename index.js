25793a7bb06d7ac97a16d80858d7cb6642127f7b
masum362 <masumahmed64077@gmail.com>
2024-06-02 15:44:31 +0600

    added basic routing api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import router from "./router/router.js";
 
 dotenv.config();
 const corsConfig = {
@@ -17,9 +18,9 @@ const app = express();
 app.use(cors(corsConfig));
 app.use(cookieParser());
 app.use(express.json());
+app.use("/", router);
 
 const port = process.env.PORT || 3000;
-app.listen(port);
-
-
-
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
