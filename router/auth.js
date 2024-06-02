de768b0ff2dc57040bdfff93558f8934a4fa81a4
masum362 <masumahmed64077@gmail.com>
2024-06-02 16:20:42 +0600

    verifyToken middleware added

---
+++ router/auth.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+
+
+const verifyToken = (req ,res ,next) => {
+    const token = req.headers?.authorization?.split(" ")[1] || '';
+
+    if(!token) res.status(403).json({message:"Invalid token"});
+    jwt.verify(token,process.env.JWT_SECRET,(error,decoded)=>{
+        if(error) res.status(403).json({message:error.message});
+        req.userId = decoded.id;
+        next();
+    })
+}
+
+
+export {verifyToken};
